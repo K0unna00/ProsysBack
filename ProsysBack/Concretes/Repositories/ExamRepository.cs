12ee7ccfe831a78895d102891f300e82359a4af9
K0unna00 <78pf4jz@code.edu.az>
2024-07-14 23:27:48 +0400

    BA

--- ProsysBack/Concretes/Repositories/ExamRepository.cs
+++ ProsysBack/Concretes/Repositories/ExamRepository.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProsysBack.Abstractions.Repositories;
 using ProsysBack.Concretes.Repositories.Base;
 using ProsysBack.DAL;
 using ProsysBack.Entities;
+using ProsysBack.Models;
 
 namespace ProsysBack.Concretes.Repositories;
 
@@ -16,5 +18,14 @@ public class ExamRepository : Repository<Exam>, IExamRepository
     public async override Task<List<Exam>> GetAllAsync()
     {
         return await _table.Include(x => x.Student).Include(x => x.Lesson).ToListAsync();
+    }
+
+    public async override Task<PaginationRs<Exam>> GetAllPaginationAsync(Pagination pagination)
+    {
+        var response = await _table.Include(x => x.Student).Include(x => x.Lesson).Skip(pagination.Page * pagination.Size).Take(pagination.Size).ToListAsync();
+
+        var totalCount = await _table.CountAsync();
+
+        return new PaginationRs<Exam> { Response = response, TotalCount = totalCount };
     }
 }
